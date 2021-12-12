935e50e846815f62e349ace2089f0fd95ce50458
Tobias Gesellchen <tobias@gesellix.de>
2021-12-12 22:22:14 +0100

    add a hijacking handler for integration tests of our docker-client

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/gesellix/go-npipe
 
 go 1.17
 
-require github.com/Microsoft/go-winio v0.5.1
+require (
+	github.com/Microsoft/go-winio v0.5.1
+	github.com/pkg/errors v0.9.1
+)
 
 require golang.org/x/sys v0.0.0-20211105183446-c75c47738b0c // indirect
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Microsoft/go-winio"
+	"github.com/pkg/errors"
 	"io"
 	"io/ioutil"
 	"log"
@@ -9,6 +11,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 func printUsage() {
@@ -49,11 +52,14 @@ func main() {
 
 func serve(l net.Listener) error {
 	http.HandleFunc("/", HandleDefault)
+	http.HandleFunc("/containers/", HandleHijacked)
+	http.HandleFunc("/hijack/", HandleHijacked)
 	http.HandleFunc("/exit", HandleExit)
 	return http.Serve(l, nil)
 }
 
 func HandleDefault(w http.ResponseWriter, req *http.Request) {
+	log.Printf("<default handler> for %s %s\n", req.Method, req.RequestURI)
 	body, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
 	if err != nil {
 		log.Panic(err)
@@ -81,4 +87,126 @@ func HandleExit(w http.ResponseWriter, req *http.Request) {
 		f.Flush()
 	}
 	defer os.Exit(0)
+}
+
+// everything below this is taken/copied/inspired from:
+// - https://github.com/moby/moby/blob/07d60bc2571ba3d680f21adc84d87803ab4959c6/daemon/attach.go
+// - https://github.com/containerd/containerd/blob/1f5b84f27cd675780bc7127f9aedbfe34cc7590b/pkg/cri/io/container_io.go#L137
+
+var chErr chan error
+
+// ParseForm ensures the request form is parsed even with invalid content types.
+// If we don't do this, POST method without Content-type (even with empty body) will fail.
+func ParseForm(r *http.Request) error {
+	if r == nil {
+		return nil
+	}
+	if err := r.ParseForm(); err != nil && !strings.HasPrefix(err.Error(), "mime:") {
+		return err
+	}
+	return nil
+}
+
+// HijackConnection interrupts the http response writer to get the
+// underlying connection and operate with it.
+func HijackConnection(w http.ResponseWriter) (io.ReadCloser, io.Writer, error) {
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		return nil, nil, err
+	}
+	// Flush the options to make sure the client sets the raw mode
+	_, _ = conn.Write([]byte{})
+	return conn, conn, nil
+}
+
+func HandleHijacked(w http.ResponseWriter, req *http.Request) {
+	log.Printf("<hijacking handler> for %s %s\n", req.Method, req.RequestURI)
+
+	chErr = make(chan error, 1)
+	defer close(chErr)
+	if err := ParseForm(req); err != nil {
+		chErr <- errors.Wrap(err, "error parsing form")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	r, rw, err := HijackConnection(w)
+	if err != nil {
+		chErr <- errors.Wrap(err, "error hijacking connection")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Close()
+
+	fmt.Fprint(rw, "HTTP/1.1 101 UPGRADED\r\nContent-Type: application/vnd.docker.raw-stream\r\nConnection: Upgrade\r\nUpgrade: tcp\r\n\n")
+
+	reader, writer := io.Pipe()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		if _, err := io.Copy(writer, r); err != nil {
+			log.Printf("Failed to pipe stdin for attach: %v\n", err)
+		}
+		log.Printf("Attach stream stdin closed\n")
+		writer.Close()
+		r.Close()
+		//rw.Close()
+		wg.Done()
+	}()
+
+	attachStream := func(key string, close <-chan struct{}) {
+		io.Copy(rw, reader)
+		<-close
+		log.Printf("Attach stream %q closed", key)
+		// Make sure stdin gets closed.
+		if r != nil {
+			r.Close()
+		}
+		wg.Done()
+	}
+
+	wg.Add(1)
+	close := make(chan struct{})
+	go attachStream("stdout", close)
+
+	wg.Wait()
+}
+
+// writeCloseInformer wraps a reader with a close function.
+type wrapReadCloser struct {
+	reader *io.PipeReader
+	writer *io.PipeWriter
+}
+
+// NewWrapReadCloser creates a wrapReadCloser from a reader.
+// NOTE(random-liu): To avoid goroutine leakage, the reader passed in
+// must be eventually closed by the caller.
+func NewWrapReadCloser(r io.Reader) io.ReadCloser {
+	pr, pw := io.Pipe()
+	go func() {
+		_, _ = io.Copy(pw, r)
+		pr.Close()
+		pw.Close()
+	}()
+	return &wrapReadCloser{
+		reader: pr,
+		writer: pw,
+	}
+}
+
+// Read reads up to len(p) bytes into p.
+func (w *wrapReadCloser) Read(p []byte) (int, error) {
+	n, err := w.reader.Read(p)
+	if err == io.ErrClosedPipe {
+		return n, io.EOF
+	}
+	return n, err
+}
+
+// Close closes read closer.
+func (w *wrapReadCloser) Close() error {
+	w.reader.Close()
+	w.writer.Close()
+	return nil
 }
